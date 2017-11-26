a9ebb6e3b362e2c454e4c9dbbf5e3591acfeb910
Quinton Weenink <quinton.weenink@standardbank.co.za>
2017-11-27 01:14:21 +0200

    New main

--- main.go
+++ main.go
@@ -1,29 +1,86 @@
 package main
 
 import (
-	"log"
 	"net/http"
+	"fmt"
 	"os"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
+	"encoding/json"
+	"io/ioutil"
 )
+
+type Fruits map[string] int
+type Vegetables map[string] int
+
+type Data struct {
+	Fruit Fruits
+	Verggies Vegetables
+}
+
+type Payload struct {
+	Stuff Data
+}
+
+
+
+func serveRest(w http.ResponseWriter, r *http.Request) {
+	response, err := getJsonResponse()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, string(response))
+}
+
+func serveRestGopher(w http.ResponseWriter, r *http.Request) {
+	response, err := getJsonResponse()
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var p Payload
+
+	err = json.Unmarshal(body, &p)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(p)
+
+	fmt.Fprintf(w, string(response))
+}
 
 func main() {
 	port := os.Getenv("PORT")
-
 	if port == "" {
-		log.Fatal("$PORT must be set")
+		port = "8080"
 	}
+	host := "localhost:" + port
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+	fmt.Println("Serving on " + host + "..")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+	http.HandleFunc("/gopher", serveRestGopher)
+	http.HandleFunc("/", serveRest)
 
-	router.Run(":" + port)
+	http.ListenAndServe(host, nil)
+}
+
+
+func getJsonResponse() ([]byte, error){
+	fruits := make(map[string] int)
+	fruits["Apples"] = 25
+	fruits["Peppers"] = 11
+
+	vegetables := make(map[string] int)
+	vegetables["Carrots"] = 21
+	vegetables["Peppers"] = 0
+
+	d := Data{fruits, vegetables}
+	p := Payload{d}
+
+	return json.MarshalIndent(p, "", "  ")
 }
