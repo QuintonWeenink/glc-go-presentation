dd75b4846dd187742d73b833a3de807ea301a3b5
Quinton Weenink <quinton.weenink@standardbank.co.za>
2017-11-27 03:05:37 +0200

    Basic stupid service

--- main.go
+++ main.go
@@ -4,10 +4,18 @@ import (
 	"net/http"
 	"fmt"
 	"os"
-	"log"
 	"encoding/json"
 	"io/ioutil"
 )
+
+type Item interface {
+
+}
+
+type Fruit struct {
+	name string
+	amount int
+}
 
 type Fruits map[string] int
 type Vegetables map[string] int
@@ -21,6 +29,12 @@ type Payload struct {
 	Stuff Data
 }
 
+type Request struct {
+	Item map[string] interface{}
+}
+
+var vegetables map[string] int = make(map[string] int)
+var fruits map[string] int = make(map[string] int)
 
 
 func serveRest(w http.ResponseWriter, r *http.Request) {
@@ -32,54 +46,57 @@ func serveRest(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(response))
 }
 
-func serveRestGopher(w http.ResponseWriter, r *http.Request) {
-	response, err := getJsonResponse()
-	if err != nil {
-		panic(err)
-	}
+func postFruit(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
 
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		panic(err)
 	}
 
-	var p Payload
+	var p Request
 
 	err = json.Unmarshal(body, &p)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(p)
+	name, ok := p.Item["name"].(string)
+	if !ok {
+		panic("Name should be string")
+	}
+	amount, ok := p.Item["amount"].(float64)
+	if !ok {
+		panic("Amount should be int")
+	}
+	fruits[name] = int(amount)
 
-	fmt.Fprintf(w, string(response))
+	fmt.Fprintf(w, string("Your fruit has been added"))
 }
 
 func main() {
+	vegetables["Carrots"] = 21
+	vegetables["Peppers"] = 0
+
+	fruits["Apples"] = 25
+	fruits["Peppers"] = 11
+
 	port := os.Getenv("PORT")
 
 	if port == "" {
-		log.Fatal("$PORT must be set")
+		port = "8080"
 	}
 	host := "localhost:" + port
 
 	fmt.Println("Serving on " + host + "..")
 
-	http.HandleFunc("/gopher", serveRestGopher)
+	http.HandleFunc("/fruit", postFruit)
 	http.HandleFunc("/", serveRest)
 
 	http.ListenAndServe(host, nil)
 }
 
 func getJsonResponse() ([]byte, error){
-	fruits := make(map[string] int)
-	fruits["Apples"] = 25
-	fruits["Peppers"] = 11
-
-	vegetables := make(map[string] int)
-	vegetables["Carrots"] = 21
-	vegetables["Peppers"] = 0
-
 	d := Data{fruits, vegetables}
 	p := Payload{d}
 
